7e0ae600a0d660b638fadf4d179d5824e7d348cb
howdo <yieron19920624>
2017-01-09 19:09:02 +0800

    Initial commit

---
+++ app/src/main/java/com/helloandroid/yieron/yieron/ui/MainActivity.java
@@ -0,0 +1,24 @@
+package com.helloandroid.yieron.yieron.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.helloandroid.yieron.yieron.R;
+
+/**
+ * 此项目为基项目，集成了很多demo，方便在实际的项目中直接使用。
+ * 同时，此项目为自己兴趣爱好和学习研究的工具项目，就像工具类一样，这是工具项目。
+ * 此项目永久保存。
+ * please contact me:15117958831
+ * email:yieron@163.com
+ * qq:2293264218
+ * 2016年10月27日
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
