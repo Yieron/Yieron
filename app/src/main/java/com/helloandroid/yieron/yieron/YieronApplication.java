c599a8904b3a1c8665167a40d80ede927330f769
howdo <yieron19920624>
2017-01-11 21:03:47 +0800

    create package

---
+++ app/src/main/java/com/helloandroid/yieron/yieron/YieronApplication.java
@@ -0,0 +1,9 @@
+package com.helloandroid.yieron.yieron;
+
+/**
+ * Created by howdo on 2017/1/11.
+ */
+
+public class YieronApplication {
+
+}
--- app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/MainActivity.java
+++ app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.helloandroid.yieron.yieron.ui;
+package com.helloandroid.yieron.yieron.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
