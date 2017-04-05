ab695a8d217ed0d932ee9fb0349e48f3b430114a
yindongdong <yieorn@163.com>
2017-04-05 10:43:05 +0800

    add some file

--- app/src/main/java/com/helloandroid/yieron/yieron/base/BaseActivity.java
+++ app/src/main/java/com/helloandroid/yieron/yieron/base/BaseActivity.java
@@ -1,12 +1,29 @@
 package com.helloandroid.yieron.yieron.base;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by howdo on 2017/1/11.
  */
 
-public class BaseActivity extends AppCompatActivity{
+public class BaseActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
