42235b304a597c58891e3264afbb79563c0e5953
Yieron东 <2293264218@qq.com>
2019-02-01 14:33:25 +0800

    【端】Yieron 【Task】添加闪屏页 【修改内容】 【后续跟进】

--- app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/FlashActivity.java
+++ app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/FlashActivity.java
@@ -1,7 +1,9 @@
 package com.helloandroid.yieron.yieron.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.helloandroid.yieron.yieron.R;
 
@@ -11,6 +13,21 @@ public class FlashActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    Intent intent = new Intent();
+                    intent.setClass(FlashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
