2f8d47820cb95cc4e2ee2e026e48fd7639f48739
Yieron东 <2293264218@qq.com>
2019-03-06 09:17:18 +0800

    【端】ReactNativeDemo 【Task】升级Android Studio 【修改内容】 【后续跟进】

--- app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/FlashActivity.java
+++ app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/FlashActivity.java
@@ -1,13 +1,17 @@
 package com.helloandroid.yieron.yieron.ui.activity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 import com.helloandroid.yieron.yieron.R;
 
 public class FlashActivity extends AppCompatActivity {
+    private static final int OVERLAY_PERMISSION_REQ_CODE = 281;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,19 +19,41 @@ public class FlashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_flash);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(3000);
-                    Intent intent = new Intent();
-                    intent.setClass(FlashActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        /**
+         * 如果你的应用会运行在Android 6.0（API level 23）或更高版本，需要动态去申请打开悬浮窗(overlay)权限
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(3000);
+                        Intent intent = new Intent();
+                        intent.setClass(FlashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
                 }
             }
-        }).start();
+        }
     }
 }
