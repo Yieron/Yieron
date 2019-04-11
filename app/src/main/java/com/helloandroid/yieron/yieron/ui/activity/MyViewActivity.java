98066fd47cb0f21353805a19fef1fe007c449747
Yieron东 <2293264218@qq.com>
2019-04-11 15:14:25 +0800

    【端】Yieron 【Task】自定义View 【修改内容】 【后续跟进】

--- app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/MyViewActivity.java
+++ app/src/main/java/com/helloandroid/yieron/yieron/ui/activity/MyViewActivity.java
@@ -9,17 +9,14 @@
 package com.helloandroid.yieron.yieron.ui.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.helloandroid.yieron.yieron.R;
 
 public class MyViewActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view);
-
     }
 }
--- yieronview/src/main/java/com/example/yieronview/YieronLayout.java
+++ yieronview/src/main/java/com/example/yieronview/YieronLayout.java
@@ -9,10 +9,27 @@
 package com.example.yieronview;
 
 import android.content.Context;
-import android.widget.FrameLayout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
 
-public class YieronLayout extends FrameLayout {
+public class YieronLayout extends RelativeLayout {
+
+    private Context mContext;
+
     public YieronLayout(Context context) {
         super(context);
+        mContext = context;
+        initView();
+    }
+
+    public YieronLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        initView();
+    }
+
+    private void initView() {
+        LayoutInflater.from(mContext).inflate(R.layout.my_view_btn, this,true);
     }
 }
