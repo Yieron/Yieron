63d1a5afa4b9af45e29da4771862d48ad72b7934
Yieron东 <2293264218@qq.com>
2019-04-19 15:25:21 +0800

    【端】Yieron 【Task】 【修改内容】 【后续跟进】

--- yieronview/src/main/java/com/example/yieronview/FallingBall.java
+++ yieronview/src/main/java/com/example/yieronview/FallingBall.java
@@ -1,25 +1,3 @@
-/*
- * Copyright 2017 GcsSloop
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Last modified 2017-07-02 17:08:46
- *
- * GitHub: https://github.com/GcsSloop
- * WeiBo: http://weibo.com/GcsSloop
- * WebSite: http://www.gcssloop.com
- */
-
 package com.example.yieronview;
 
 import android.content.Context;
@@ -41,7 +19,7 @@ import android.view.View;
 /**
  * 可以拖动的 Ball
  */
-public class FalingBall extends View {
+public class FallingBall extends View {
     private int mWidth;             // 宽度
     private int mHeight;            // 高度
 
@@ -66,15 +44,15 @@ public class FalingBall extends View {
 
     private Bitmap mBitmap;
 
-    public FalingBall(Context context) {
+    public FallingBall(Context context) {
         this(context, null);
     }
 
-    public FalingBall(Context context, @Nullable AttributeSet attrs) {
+    public FallingBall(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public FalingBall(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public FallingBall(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mGestureDetector = new GestureDetector(context, mSimpleOnGestureListener);
 
@@ -82,7 +60,7 @@ public class FalingBall extends View {
         mPaint.setColor(Color.BLACK);
         mPaint.setAntiAlias(true);
 
-        mBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ball);
+        mBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dear);
     }
 
     @Override
